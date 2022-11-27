5eb6bd8e3c5e9ceb3878055111841383c0b549c8
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2022-11-27 21:32:12 +0530

    v1.0.0

---
+++ logger.js
@@ -0,0 +1,16 @@
+const moment = require('moment')
+
+
+exports.handler = async(event) =>{
+
+    var resp = {
+        message: "Returning from hell like a pro.",
+        version: "v1.0.0",
+        time: moment().format('MMMM Do YYYY, h:mm:ss a')
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(resp)
+    }
+}
